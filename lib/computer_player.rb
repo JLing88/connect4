6b72d5b7816b67fb35fd149540e90f281560a988
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-15 20:21:48 -0600

    Begins computer_player class

---
+++ lib/computer_player.rb
@@ -0,0 +1,20 @@
+class ComputerPlayer
+
+  def generate_column
+    random_number = rand(1..7)
+    case random_number
+    when 1
+      "A"
+    when 2
+      "B"
+    when 3
+      "C"
+    when 4
+      "D"
+    when 5
+      "E"
+    when 6
+      "F"
+
+  end
+end
