2aeb59101eb9cecbede5cc5e67de1ac48849f530
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-15 21:23:20 -0600

    Moves game_loop to the Game class

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -3,24 +3,20 @@ class ComputerPlayer
   def generate_column
     random_number = rand(1..7)
     case random_number
-    when 1
-      "A"
-    when 2
-      "B"
-    when 3
-      "C"
-    when 4
-      "D"
-    when 5
-      "E"
-    when 6
-      "F"
-<<<<<<< HEAD
-
-=======
-    when 7
-      "G"
+      when 1
+        "A"
+      when 2
+        "B"
+      when 3
+        "C"
+      when 4
+        "D"
+      when 5
+        "E"
+      when 6
+        "F"
+      when 7
+        "G"
     end
->>>>>>> 794412f0def136034e5641c7666853b39f96cfbd
   end
 end
--- lib/connect4.rb
+++ lib/connect4.rb
@@ -1,12 +1,3 @@
 require './lib/game.rb'
 
 game = Game.new
-
-puts game.welcome_message
-
-puts "Player, what is your name? "
-game.name = gets.chomp
-
-loop until game.game_over == true
-
-end
